3555bf005c5a6e873f37f62b4aac555e5887ed2e
Howard Beck <howard.beck314@gmail.com>
2020-03-22 00:43:51 -0700

    Putting all raw data into object form

--- characterization-analysis/src/main/java/analysis/Analyze.java
+++ characterization-analysis/src/main/java/analysis/Analyze.java
@@ -17,5 +17,18 @@ public class Analyze {
 
         ArrayList<CharacterizationData> data = new ArrayList<CharacterizationData>();
 
+        add(dataSet.fastBackward, data);
+        add(dataSet.fastForward, data);
+        add(dataSet.slowBackward, data);
+        add(dataSet.slowForward, data);
+        add(dataSet.trackWidth, data);
+
+
+    }
+
+    public static void add(double[][] raw, ArrayList<CharacterizationData> data) {
+        for (int i = 0; i < raw.length; i++) {
+            data.add(new CharacterizationData(raw[i]));
+        }
     }
 }
